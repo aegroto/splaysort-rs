b3972256d63ad34b732ec4c7d633437aafd32c46
aegroto <aegroto@protonmail.com>
2020-07-11 11:46:23 +0200

    added zig and zag operations with single child tests

--- src/splay/tests.rs
+++ src/splay/tests.rs
@@ -25,24 +25,43 @@ fn test_zig() {
 }
 
 #[test]
-fn test_zag() {
+fn test_zig_with_left_child() {
     // Setup
     let mut splay_tree = default_start_tree();
 
-    splay_tree.insert(3);
     splay_tree.insert(5);
+    splay_tree.insert(2);
+    splay_tree.insert(1);
 
     // Exercise
-    splay_tree.splay(5);
+    splay_tree.splay(2);
 
     // Verification
-    let expected_tree = "TopDownSplayTree { root: Some(SplayNode { key: 5, left: Some(SplayNode { key: 3, left: None, right: None }), right: None }) }";
+    let expected_tree = "TopDownSplayTree { root: Some(SplayNode { key: 2, left: Some(SplayNode { key: 1, left: None, right: None }), right: Some(SplayNode { key: 5, left: None, right: None }) }) }";
 
     assert_eq!(format!("{:?}", splay_tree), expected_tree);
 }
 
 #[test]
-fn test_zig_with_children() {
+fn test_zig_with_right_child() {
+    // Setup
+    let mut splay_tree = default_start_tree();
+
+    splay_tree.insert(5);
+    splay_tree.insert(2);
+    splay_tree.insert(3);
+
+    // Exercise
+    splay_tree.splay(2);
+
+    // Verification
+    let expected_tree = "TopDownSplayTree { root: Some(SplayNode { key: 2, left: None, right: Some(SplayNode { key: 5, left: Some(SplayNode { key: 3, left: None, right: None }), right: None }) }) }";
+
+    assert_eq!(format!("{:?}", splay_tree), expected_tree);
+}
+
+#[test]
+fn test_zig_with_both_children() {
     // Setup
     let mut splay_tree = default_start_tree();
 
@@ -61,7 +80,60 @@ fn test_zig_with_children() {
 }
 
 #[test]
-fn test_zag_with_children() {
+fn test_zag() {
+    // Setup
+    let mut splay_tree = default_start_tree();
+
+    splay_tree.insert(3);
+    splay_tree.insert(5);
+
+    // Exercise
+    splay_tree.splay(5);
+
+    // Verification
+    let expected_tree = "TopDownSplayTree { root: Some(SplayNode { key: 5, left: Some(SplayNode { key: 3, left: None, right: None }), right: None }) }";
+
+    assert_eq!(format!("{:?}", splay_tree), expected_tree);
+}
+
+#[test]
+fn test_zag_with_left_child() {
+    // Setup
+    let mut splay_tree = default_start_tree();
+
+    splay_tree.insert(3);
+    splay_tree.insert(5);
+    splay_tree.insert(4);
+
+    // Exercise
+    splay_tree.splay(5);
+
+    // Verification
+    let expected_tree = "TopDownSplayTree { root: Some(SplayNode { key: 5, left: Some(SplayNode { key: 3, left: None, right: Some(SplayNode { key: 4, left: None, right: None }) }), right: None }) }";
+
+    assert_eq!(format!("{:?}", splay_tree), expected_tree);
+}
+
+#[test]
+fn test_zag_with_right_child() {
+    // Setup
+    let mut splay_tree = default_start_tree();
+
+    splay_tree.insert(3);
+    splay_tree.insert(5);
+    splay_tree.insert(7);
+
+    // Exercise
+    splay_tree.splay(5);
+
+    // Verification
+    let expected_tree = "TopDownSplayTree { root: Some(SplayNode { key: 5, left: Some(SplayNode { key: 3, left: None, right: None }), right: Some(SplayNode { key: 7, left: None, right: None }) }) }";
+
+    assert_eq!(format!("{:?}", splay_tree), expected_tree);
+}
+
+#[test]
+fn test_zag_with_both_children() {
     // Setup
     let mut splay_tree = default_start_tree();
 
